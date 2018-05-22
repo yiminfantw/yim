8897797a566ecc561f457525abab197b6ad2c1cb
YiMin Fan <yf@renovatiopictures.com>
2018-05-22 01:01:09 -0700

    Added setup.sh, indentline_vimrc.

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# -----------------------
+# Define local variables.
+#
+bin_dirpath=`dirname $BASH_SOURCE`/bin
+
+# -----------------------------
+# Define environment variables.
+#
+export PATH=$PATH:$bin_dirpath
