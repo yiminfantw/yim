8147e5f59e15e79f03393ed060bb406b12df0314
YiMin Fan <yf@renovatiopictures.com>
2018-06-27 23:46:46 +0800

    Added doc.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,18 @@
+##
+## This script sets up env vars for running yim.
+##
+## This script will be sourced by /rp-opt/rp-bash in a running rp-dev container.
+##
+
+
 # -----------------------
 # Define local variables.
 #
 bin_dirpath=`dirname $BASH_SOURCE`/bin
 
+
 # -----------------------------
 # Define environment variables.
 #
 export PATH=$PATH:$bin_dirpath
+
